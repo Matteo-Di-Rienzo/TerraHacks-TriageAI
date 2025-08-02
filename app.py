c37639d8ee33b48209892ddc0096d36a71747e27
Yatri Rathod <yatriba.rathod@gmail.com>
2025-08-02 15:02:35 -0400

    updated to vellum ai

--- app.py
+++ app.py
@@ -3,21 +3,22 @@ from pymongo import MongoClient
 from bson import ObjectId
 from dotenv import load_dotenv
 import os
-import google.generativeai as genai
+import json
+from vellum.client import Vellum
+import vellum.types as types
+
 
 load_dotenv()
 
 app = Flask(__name__)
 mongo_uri = os.getenv("MONGO_URI")
-api_key = os.getenv("GEMINI_API_KEY")
+vellum_api_key = os.getenv("VELLUM_API_KEY")
 
 
 client = MongoClient(mongo_uri)
 db = client["terrahacks"]
 patientsCollection = db["patients"]
-patients = list(patientsCollection.find({}))
-
-
+patients = list(patientsCollection.find({}, {"_id": 0}))  # Exclude _id field
 
 @app.route("/")
 def index():
@@ -64,13 +65,26 @@ def update_patient():
     return redirect("/patients")
 
 
-@app.route("/gemini", methods=["POST"])
-def gemini():  
-  # -------------To be edited by Prithvi--------------
-  prompt = f"Here is the patient data: {patients}. What insights can you provide?"
 
-  # Authenticate with Gemini
-  genai.configure(api_key)
-  model = genai.GenerativeModel("gemini-pro")
-  response = model.generate_content(prompt)
-  return jsonify({"gemini_response": response.text})
+# pip install vellum-ai
+client = Vellum(
+  api_key=os.getenv["VELLUM_API_KEY"]
+)
+# patients_data = list(patientsCollection.find({}, {"_id": 0}))  # Get all patients, exclude _id
+value_patients=json.dumps(patients)  # Convert to JSON string
+
+result = client.execute_prompt(
+    prompt_deployment_name="terra-hacks-er-triage-system-variant-1",
+    release_tag="LATEST",
+    inputs=[
+        types.StringInputRequest(
+            name="patients_list",
+            value=value_patients,
+        ),
+    ],
+)
+
+if result.state == "REJECTED":
+    raise Exception(result.error.message)
+
+print(result.outputs)
