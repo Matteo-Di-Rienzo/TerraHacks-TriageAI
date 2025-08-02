7d04479fd19ffa41f5864bcdd459a3f796c420ce
Matteo-Di-Rienzo <Matt23.hicks@gmail.com>
2025-08-02 12:53:19 -0400

    Implement delete function

--- static/script.js
+++ static/script.js
@@ -3,15 +3,21 @@ const patientForm = document.getElementById("patient-form")
 
 patientForm.addEventListener("submit", e => {
   e.preventDefault();
-  
+
   const name = document.getElementById("name").value;
   const age = parseInt(document.getElementById("age").value);
+  const height = parseInt(document.getElementById("height").value);
+  const weight = parseInt(document.getElementById("weight").value);
+  const incidentTime = document.getElementById("incidentTime").value;
+  const admittanceTime = document.getElementById("admittanceTime").value;
   const concern = document.getElementById("concern").value;
 
   fetch("/add", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
-    body: JSON.stringify({ name, age, concern })
+    body: JSON.stringify({ name, age, height, weight, 
+                          incidentTime, admittanceTime,  
+                          concern})
   })
     .then(res => res.json())
     .then(() => {
