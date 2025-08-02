422585857c423a83c3eec3627e03238e95dbc5bd
Matteo-Di-Rienzo <Matt23.hicks@gmail.com>
2025-08-02 10:02:27 -0400

    Remove extra flask app, restructure folders to accomodate a simple one port flask setup.

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
-from flask import Flask, render_template, request
+from flask import Flask, jsonify, request
+from pymongo import MongoClient
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 app = Flask(__name__)
+mongo_uri = os.getenv("MONGO_URI")
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    submitted = False
-    data = {}
-    if request.method == 'POST':
-        data['name'] = request.form.get('name')
-        data['color'] = request.form.get('color')
-        submitted = True
-    return render_template('index.html', submitted=submitted, data=data)
+client = MongoClient(mongo_uri)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+@app.route("/")
+def home():
+  return f"Connected: {mongo_uri}"
+
+
+@app.route("/insert", methods=["GET"])
+def insert_sample():
+    test_doc = {"name": "Test", "Test": True}
+    result = client["terrahacks"]["test"].insert_one(test_doc)
+    return jsonify({"inserted_id": str(result.inserted_id)})
